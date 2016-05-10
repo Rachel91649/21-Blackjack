c29a3b4b28dae6ca53774d3d0392c02fb1e26329
Rachel Jackson <racheljackson@Rachels-MacBook-Pro.local>
2016-05-10 19:14:31 -0400

    created deck of cards

--- app.js
+++ app.js
@@ -5,8 +5,6 @@
 		// -do I have to create a deck before I can shuffle it...duh! how can you shuffle something you don't have. 
 			//-ok, so create a deck of 52 cards, 4 suits, 13cards to each suit. #cards are 2-10, then J, Q, K, A
 				//-what would the array look like: 
-				var deck = [["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"], ["C", "H", "D", "S"]];
-				console.log(deck);
 			//-shuffle cards. 
 //  2. deal to player
 		//-how does the computer know to deal?
@@ -39,3 +37,43 @@ $stay.click(function(){
 	console.log("stay clicked");
 });
 
+//========= Making the Deck ===========//
+
+var cards =["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];//array of card numbers
+var suits = ["Clubs", "Hearts", "Diamonds", "Spades"];//array of card suits
+var deck = [];//empty array where new deck will be pushed into
+
+var newDeck = function(){
+	for (var c = 0; c < cards.length; c++) {//1st loop will iterate through the card numbers for the length of the array, hitting every string.
+		for (var s = 0; s < suits.length; s++) {//2nd loop will iterate through suits for every time the 1st loop iterates through card numbers. suits has to be the 2nd loop because for every 13 cards I need a suit, essentially 13*4 = 52 cards.
+			var card = { suit: "", number: "" };//create each card as an object. The object will allow me to access the suit and number of the card separately instead of the suit and number being all together. I need this in order to be able to assign a value to the numbers.
+			card.suit = suits[s];//assign a suit to each card
+			card.number = cards[c];//assign a number to each card
+			console.log(card);//confirm I had a card with a number and suit for ever card needed and no duplicates
+			deck.push(card);//push my new cards into the array called deck
+		}
+	}
+	console.log(deck);//prints my new deck
+};
+
+newDeck();//invoke the function newDeck
+//============= Shuffle the Deck =============//
+
+
+//============== My Scrap Code =================//
+
+//========makeing the deck===========//
+// var deck = [["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"], ["C", "H", "D", "S"]];
+// console.log(deck); I created a 2D array that i want to combine into a 1D array to make a deck of 52 cards.  How do I combine it?
+// var deck = {
+// 	cards: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
+// 	suits: ["C", "H", "D", "S"]
+//};
+//var cards =["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+//var suits = ["C", "H", "D", "S"];
+// var newDeck = for (var c = 0; c < cards.length; c++) {
+// 	for(var s = 0; s < suits.lenth; s++) {
+// 		return cards[c] + suits[s]
+// 	}
+// }
+//console.log(newDeck);
