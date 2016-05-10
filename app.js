ae7e59a776fd0c8a47edb900569cf41ced100385
Rachel Jackson <racheljackson@Rachels-MacBook-Pro.local>
2016-05-10 15:53:02 -0400

    added some pc to js, started layout in html and css

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("testing");
+//console.log("testing");
+//=====Basic 21 game-pseudocode======//
+//  1. cards to need shuffle
+// 	-need shuffle function: how can you do this? 			an array. There are 52 cards in a deck, 4				  suits, 52/4 = 13. 
+		// -do I have to create a deck before I can shuffle it...duh! how can you shuffle something you don't have. 
+			//-ok, so create a deck of 52 cards, 4 suits, 13cards to each suit. #cards are 2-10, then J, Q, K, A
+			//-shuffle cards. 
+//  2. deal to player
+// 	-two cards get dealt
+// 3. player can hit or stay
+// 	-if hit clicked, deal 1 card from deck
+// 4. check for winner
+// 	-what is a winner?
+// 		-hand total == 21;
+// 		-hand total < 21, but higher than all other hands
+
+
